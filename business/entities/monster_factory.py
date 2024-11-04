4a91039989e62b58ac5fc152c93d136c9963e901
Galo <nebulafa@gmail.com>
2024-11-04 01:00:41 -0300

    added monster types

--- business/entities/monster_factory.py
+++ business/entities/monster_factory.py
@@ -12,26 +12,39 @@ class MonsterFactory:
         self.default_attack_range = default_attack_range
         self.default_max_health = default_max_health
 
-    def create_zombie(self, src_x: int, src_y: int, sprite: Sprite) -> Monster:
+    def create_monster(self, src_x: int, src_y: int, sprite: Sprite, monster_type: str):
+        """ Create monsters. """
+        if monster_type == "zombie":
+            monster = self.create_zombie(src_x, src_y, sprite, monster_type)
+        elif monster_type == "skeleton":
+            monster = self.create_skeleton(src_x, src_y, sprite, monster_type)
+        elif monster_type == "orc":
+            monster = self.create_orc(src_x, src_y, sprite, monster_type)
+        elif monster_type == "werewolf":
+            monster = self.create_werewolf(src_x, src_y, sprite, monster_type)
+        
+        return monster
+
+    def create_zombie(self, src_x: int, src_y: int, sprite: Sprite, monster_type: str) -> Monster:
         """Creates zombie monster."""
-        monster = Monster(src_x, src_y, sprite, self.default_health, self.default_max_health, self.default_damage, self.default_attack_range)
+        monster = Monster(src_x, src_y, sprite, self.default_health, self.default_max_health, self.default_damage, self.default_attack_range, monster_type)
 
         return monster
 
-    def create_skeleton(self, src_x: int, src_y: int, sprite: Sprite) -> Monster:
+    def create_skeleton(self, src_x: int, src_y: int, sprite: Sprite, monster_type: str) -> Monster:
         """Creates orc monster."""
-        monster = Monster(src_x, src_y, sprite, 15, 15, 10, 50)
+        monster = Monster(src_x, src_y, sprite, 15, 15, 10, 50, monster_type)
 
         return monster
 
-    def create_orc(self, src_x: int, src_y: int, sprite: Sprite) -> Monster:
+    def create_orc(self, src_x: int, src_y: int, sprite: Sprite, monster_type: str) -> Monster:
         """Creates orc monster."""
-        monster = Monster(src_x, src_y, sprite, 20, 20, 15, 60)
+        monster = Monster(src_x, src_y, sprite, 20, 20, 15, 60, monster_type)
 
         return monster
 
-    def create_werewolf(self, src_x: int, src_y: int, sprite: Sprite) -> Monster:
+    def create_werewolf(self, src_x: int, src_y: int, sprite: Sprite, monster_type: str) -> Monster:
         """Creates werewolf monster."""
-        monster = Monster(src_x, src_y, sprite, 25, 25, 20, 60)
+        monster = Monster(src_x, src_y, sprite, 25, 25, 20, 60, monster_type)
 
         return monster
